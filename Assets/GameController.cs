1f60b0004184cca6a3ff99b6adb6fbdc7a0684fc
carl <carl.sporrong@gmail.com>
2020-02-07 22:07:51 +0100

    first upload

---
+++ Assets/GameController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameDisplay gameDisplay;
+    public GameObject[] player;
+    int z;
+
+    public static int whereIsTheWaitress;
+
+   
+    void setFalse()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            player[i].SetActive(false);
+
+
+        }
+        player[0].SetActive(true);
+
+    }
+
+
+    void Start()
+    {
+        setFalse();
+        z = 0;
+    }
+    void Update()
+    {
+
+            
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (z < 0)
+            {
+                z = 0;
+            }
+            else if (z > 0)
+            {
+                z--;
+                player[(z + 1)].SetActive(false);
+            }
+            player[z].SetActive(true);
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (z > 4)
+            {
+                z = 4;
+            }
+            else if(z < 4)
+            {
+                z++;
+                player[(z - 1)].SetActive(false);
+            }
+
+            player[z].SetActive(true);
+
+        }
+        whereIsTheWaitress = z;
+        
+    }
+}
---
+++ Assets/GameDisplay.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDisplay : MonoBehaviour
+{
+
+    public GameController gameController;
+    
+    public GameObject[] pancakes;
+
+    public Text textScore;
+
+    int megaScore = 0;
+
+    int pP;
+    
+
+    int q;
+ 
+    void setFalse()
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            pancakes[i].SetActive(false);
+        }
+
+    }
+
+    private int setPlayerPos()
+    {
+        pP = GameController.whereIsTheWaitress;
+        return pP;
+    }
+
+    private void RandomZ()
+    {
+        int[] z = new int[] { 0, 5, 10, 15, 20, 25 };
+        int randomZ = z[Random.Range(0, z.Length)];
+        q = randomZ;
+    }
+    void Start()
+    {
+        setFalse();
+     
+
+        StartCoroutine(RandomFalling());
+
+
+    }
+   
+ 
+    IEnumerator RandomFalling()
+    {
+        while (true)
+        {
+            RandomZ();
+            
+
+            
+
+            if (q == 0 || q == 5 || q == 10 || q == 15 || q == 20)
+            {
+                for (int x = q; x < (q + 5); x++)
+                {
+                    
+                    pancakes[x].SetActive(true);
+                    yield return new WaitForSeconds(0.5f);
+
+                    pancakes[x].SetActive(false);
+
+                    setPlayerPos();
+                    if (x == 4 && pP == 0 )
+                    {
+                        megaScore++;
+                        textScore.text = megaScore.ToString();
+                    }
+
+                }
+            }  
+
+        
+        }
+    }
+}
